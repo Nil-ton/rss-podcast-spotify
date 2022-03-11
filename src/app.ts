6e7a076641354aa5bed25cd5de47a40dd62754f8
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2022-03-11 08:22:57 -0300

    src/app.ts/install and configuration cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import { router } from './routes/router'
+import cors from 'cors'
 
 export const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
